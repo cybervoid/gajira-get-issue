22794e3f84b99597a8423225e5ba28366bee4391
Rafael Gil <rafaelgil83@gmail.com>
2020-03-25 20:33:15 -0400

    - add workflow

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('input-text');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const inputText = core.getInput('input-text');
+    let regex = new RegExp('.+\\/(.*-\\d+)', 'gim');
+    let issue = regex.exec(inputText);
+
+    if (issue && Array.isArray(issue)) {
+      console.log("Issue found, Jira number: " + issue[1]);
+    } else {
+        console.log("Issue not found");
+    }
+
+    // console.log(`Input text: ${inputText}!`);
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
