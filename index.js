779738bfd70603054e3750f5c1f4cbc650c56cf9
Rafael Gil <rafaelgil83@gmail.com>
2020-03-26 23:47:49 -0400

    - creates jira config file

--- index.js
+++ index.js
@@ -3,46 +3,46 @@ const github = require('@actions/github');
 const fs = require('fs');
 
 // Get the JSON webhook payload for the event that triggered the workflow
-const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
+// const payload = JSON.stringify(github.context.payload, undefined, 2)
+// console.log(`The event payload: ${payload}`);
+// console.log('Element is ', github.context.payload.commits.head.ref);
 
-
+const inputText = core.getInput('input-text');
 try {
-    const inputText = core.getInput('input-text');
-    let regex = new RegExp('.+\\/(.*-\\d+)', 'gim');
-    let issue = regex.exec(inputText);
-
-    if (issue && Array.isArray(issue)) {
-        console.log("Issue found, Jira number: " + issue[1]);
-        core.setOutput("issue", issue[1]);
-        const filePath = require('os').homedir() + '/jira/';
-        const fileName = 'config.yml';
-        try {
-            fs.mkdirSync(filePath, {recursive: true});
-            fs.appendFileSync(filePath + fileName, 'issue: ' + issue[1] + '\r\n')
-        } catch (e) {
-            core.setFailed('Error trying to create the file ' + fileName);
-        }
+    const issue = findIssue(inputText);
+    if (issue) {
+        issueFound(issue[1]);
     } else {
         console.log("Issue not found on provided input: " + inputText);
         //try to search in github object
-        const message = payload.commits.message;
-        issue = regex.exec(message);
-        if (issue && Array.isArray(issue)) {
-            console.log("Issue found in github object, Jira number: " + issue[1]);
-            core.setOutput("issue", issue[1]);
-            const filePath = require('os').homedir() + '/jira/';
-            const fileName = 'config.yml';
-            try {
-                fs.mkdirSync(filePath, {recursive: true});
-                fs.appendFileSync(filePath + fileName, 'issue: ' + issue[1] + '\r\n')
-            } catch (e) {
-                core.setFailed('Error trying to create the file ' + fileName);
+        const commitMessages = github.context.payload.commits;
+        let res = commitMessages.some( function (element) {
+            const issue = findIssue(element.message);
+            if (issue){
+                issueFound(issue[1]);
+                return true;
             }
-        } else {
-            core.setFailed("");
-        }
+        });
+        console.log("Pepe", res);
     }
 } catch (error) {
     core.setFailed(error.message);
+}
+
+function findIssue(text) {
+    let regex = new RegExp('.+\\/(.*-\\d+)', 'gim');
+    return  regex.exec(text);
+}
+
+function issueFound(issue) {
+    console.log("Issue found, Jira number: " + issue);
+    core.setOutput("issue", issue);
+    const filePath = require('os').homedir() + '/jira/';
+    const fileName = 'config.yml';
+    try {
+        fs.mkdirSync(filePath, {recursive: true});
+        fs.appendFileSync(filePath + fileName, 'issue: ' + issue[1] + '\r\n')
+    } catch (e) {
+        core.setFailed('Error trying to create the file ' + fileName);
+    }
 }
